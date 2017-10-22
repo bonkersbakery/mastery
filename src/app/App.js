21fbab50a7a1589bcbaee3faffd103f34e32baac
daven <Daven Paul Casia>
2017-10-22 02:15:01 -0600

    MASTER: updated the template

--- src/app/App.js
+++ src/app/App.js
@@ -14,8 +14,18 @@ const styles = theme => ({
     justifyContent: "center",
     flexWrap: "wrap"
   },
+  gridContainer: {
+    paddingTop: 16,
+    display: "grid",
+    gridColumnGap: "20px",
+    alignItem: "center",
+    minHeight: 200
+  },
+  descrptionContainer: {
+    gridTemplateColumns: "1fr 1fr"
+  },
   header: {
-    height: 700,
+    height: 600,
     padding: 50,
     background: "black"
   },
@@ -23,7 +33,7 @@ const styles = theme => ({
     alignItems: "center"
   },
   description: {
-    color: "white"
+    margin: 20
   },
   content: theme.mixins.gutters({
     paddingTop: 16,
@@ -40,27 +50,89 @@ const App = ({ classes }) => (
       </div>
     </Paper>
     <div className={classes.flexCenter}>
+      <div>
+        <Paper className={classes.content} elevation={2}>
+          <Typography type="headline" component="h3">
+            Yeah doug we make cupcakes
+          </Typography>
+          <Typography type="body1" component="p">
+            Ya'll cravin for a cupcake? We got you ass covered
+          </Typography>
+        </Paper>
+        <Paper className={classes.content} elevation={2}>
+          <Typography type="headline" component="h3">
+            Yeah doug we make cupcakes
+          </Typography>
+          <Typography type="body1" component="p">
+            Ya'll cravin for a cupcake? We got you ass covered
+          </Typography>
+        </Paper>
+        <Paper className={classes.content} elevation={2}>
+          <Typography type="headline" component="h3">
+            Yeah doug we make cupcakes
+          </Typography>
+          <Typography type="body1" component="p">
+            Ya'll cravin for a cupcake? We got you ass covered
+          </Typography>
+        </Paper>
+      </div>
+      <Paper className={classes.content} elevation={2}>
+        <ImageGallery />
+      </Paper>
+    </div>
+    <div className={`${classes.gridContainer} ${classes.descrptionContainer}`}>
+      <Paper elevation={5}>
+        <div className={classes.description}>
+          <Typography type="headline" component="h3">
+            So you want some gluten-free cupcakes?
+          </Typography>
+          <Typography type="body1" component="p">
+            Then you're damn lucky, because our cupcakes are gluten-free,
+            sugar-free, tax-free, sketchy-shit-stuff-that-might-kill-you-free
+          </Typography>
+        </div>
+      </Paper>
+      <Paper elevation={5}>
+        <div className={classes.description}>
+          <Typography type="headline" component="h3">
+            Yeah doug we make cupcakes
+          </Typography>
+        </div>
+      </Paper>
+    </div>
+    <div className={classes.flexCenter}>
+      <Paper className={classes.content} elevation={2}>
+        <ImageGallery />
+      </Paper>
       <Paper className={classes.content} elevation={2}>
         <Typography type="headline" component="h3">
           Yeah doug we make cupcakes
         </Typography>
         <Typography type="body1" component="p">
-          Ya'll cravin for a cupcake? We got you ass covered 
+          Ya'll cravin for a cupcake? We got you ass covered
         </Typography>
       </Paper>
-      <Paper className={classes.content} elevation={2}>
-        <ImageGallery />
+    </div>
+    <div className={classes.gridContainer}>
+      <Paper elevation={5}>
+        <div className={classes.description}>
+          <Typography type="headline" component="h3">
+            So you want some gluten-free cupcakes?
+          </Typography>
+          <Typography type="body1" component="p">
+            Then you're damn lucky, because our cupcakes are gluten-free,
+            sugar-free, tax-free, sketchy-shit-stuff-that-might-kill-you-free
+          </Typography>
+        </div>
+      </Paper>
+      <Paper elevation={5}>
+        <div className={classes.description}>
+          <Typography type="headline" component="h3">
+            Yeah doug we make cupcakes
+          </Typography>
+        </div>
       </Paper>
     </div>
-    <Paper className={classes.content} elevation={2}>
-      <Typography type="headline" component="h3">
-        This is a sheet of paper.
-      </Typography>
-      <Typography type="body1" component="p">
-        Paper can be used to build surface or other elements for your
-        application.
-      </Typography>
-    </Paper>
   </div>
 );
 
